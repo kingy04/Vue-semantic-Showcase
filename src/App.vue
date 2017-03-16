8a0219205ef501c590389ff35a5a213d3c2a609e
Antony K <antonyking@Antonys-MacBook-Pro.local>
2017-03-16 10:33:26 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <div class="main ui container">
+      <h2 class="ui dividing header">Semantic Elements:<a class="anchor" id="types"></a></h2>
+
+        <accordion></accordion>
+
+        <semantic-example title="Form Dropdown" :exampleCode="formDropdownCode">
+          <semantic-form-dropdown placeholder="Placeholder text" :options="options" :value="someValue" class="example"></semantic-form-dropdown>
+        </semantic-example>
+
+        <semantic-example title="Dropdown with freetext search" :exampleCode="formDropdownCode">
+          <semantic-dropdown class="example" full-text-search="true">
+            <div class="item">one</div>
+            <div class="item">two</div>
+            <div class="item">three</div>
+          </semantic-dropdown>
+        </semantic-example>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion';
+
+export default {
+  name: 'app',
+  components: {
+    Accordion,
+  },
+  data() {
+    return {
+      options: [
+        { id: 1, name: 'one' },
+        { id: 2, name: 'two' },
+        { id: 3, name: 'three' },
+      ],
+      someValue: 1,
+      formDropdownCode: '<semantic-form-dropdown placeholder="Placeholder text" :options="options" :value="someValue"></semantic-form-dropdown>',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <semantic-example title="Accordion" :exampleCode="accordionCode">
+    <semantic-checkbox type="toggle" label="Styled" v-model="styled"></semantic-checkbox>
+    <semantic-checkbox type="toggle" label="Fluid" v-model="fluid"></semantic-checkbox>
+    <semantic-accordion :styled="styled" :fluid="fluid" :items="accordionItems" class="example"></semantic-accordion>
+    </semantic-example>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accordionItems: [
+        { title: 'What is a dog', content: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.' },
+        { title: 'two', content: 'test two' },
+      ],
+      styled: true,
+      fluid: true,
+    };
+  },
+  computed: {
+    accordionCode() {
+      return `<semantic-accordion :items="accordionItems" ${this.fluid ? '' : ':fluid="false"'} ${!this.styled ? ':styled="false"' : ''}></semantic-accordion>`;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
